238ab1f8b6610a67ca16994b308048b6a58993c4
hongtao-argmin <hong.tao@ieee.org>
2025-11-24 01:06:11 +0000

    Deploying to gh-pages from @ xu-zhaoyi/xu-zhaoyi.github.io@ce7254989c88ccd008e933a385b6b3af7ed4b6d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,13 +472,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/zhaoyi-xu-434169179", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
