f5612e6f6b323799c1803a2af277a530f10bd196
hongtao-argmin <hong.tao@ieee.org>
2025-11-24 01:05:22 +0000

    Deploying to gh-pages from @ xu-zhaoyi/xu-zhaoyi.github.io@8f7105dc7e461ccf17cdcae67717f24fd26fda9f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -465,11 +465,11 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/zhaoyi-xu-434169179", "_blank");
         },
       },{
         id: 'social-rss',
@@ -483,14 +483,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=5ssuD2UAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
