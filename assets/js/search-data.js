d0f872379ac7dd6400f238a2288ad7c7e2bb2ba1
xu-zhaoyi <116219868+xu-zhaoyi@users.noreply.github.com>
2025-12-03 03:00:50 +0000

    Deploying to gh-pages from @ xu-zhaoyi/xu-zhaoyi.github.io@db1910fcd219f43fd5c2d3ea77cab9ed7f29d3fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,14 +466,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Zhaoyi_resume_251120.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%68%61%6F%79%69%78@%75%6D%69%63%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-linkedin',
