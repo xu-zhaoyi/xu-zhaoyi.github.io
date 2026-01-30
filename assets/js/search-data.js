55192601851a82edf767b514a31ea222cb05907f
xu-zhaoyi <116219868+xu-zhaoyi@users.noreply.github.com>
2026-01-30 04:55:05 +0000

    Deploying to gh-pages from @ xu-zhaoyi/xu-zhaoyi.github.io@116f53322c4e32902b3a8e4fd5cf97519dc2edb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
